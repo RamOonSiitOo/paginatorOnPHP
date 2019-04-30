9b30ae14e9bf86bbe840254e8d3623ca6288051a
Ramon B.C <36272095+RamOonSiitOo@users.noreply.github.com>
2019-04-30 15:27:05 +0200

    Update paginator.php
    Fixed the problem when the page has no results. Just have to check on Front where you have to take the first position if is null or not, anyway the second index position give you always the last page. PD: On Front before render the php file, if the current page where we are, is more than the last one we got, just redirect to the last page like this: 
    
    $lastPage = $paginationData[1];//LAST PAGE VARIABLE
    if($currentPage>$lastPage){
        header ("Location: ?page=".$lastPage);
    }

--- paginator.php
+++ paginator.php
@@ -5,15 +5,13 @@
 *   -Current Page we're requesting
 * How works: 
 *   Check if current page number is less than total pages and no 0.
-*     If -> just fill everything, if there are results, return results, if not, just empty not null.
-*     Else -> just redirect to the last page that has results and RETURN null (That can helps you to know if we gonna keep the current 
-*             request page or request another one to get a properly results like when you're out of index and want to go to the last page 
-*             or the first one.
+*     If -> just fill everything, all the data on the current page at first index position and last page on second one
+*     Else -> just fill the array with null on first index position and last page on second one
 *   Returns: Array called paginationData with 2 positions (if it's filled): [[all_data_to_page],[last_page]]
 **/
 
 function getPaginationData ($data, $resultNum, $currentPage){
-    //Array Size 2 [[Array data], last page]
+    //Array(paginationData) Size: 2 [[Array data], last page]
     $paginationData = array();
     $lastPage = floor(sizeof($data)/$resultNum);
     $numResultsLastPage = sizeof($data)%$resultNum;
@@ -24,7 +22,8 @@ function getPaginationData ($data, $resultNum, $currentPage){
     if($lastPage!=0 && $lastPage>=$currentPage){
         $initialPosition = isset($currentPage) ? ($currentPage-1) * $resultNum : 0;
         $lastCounterPosition = $initialPosition+$resultNum;
-  
+
+        //If there is a problem here, just is because the url is manually introduced, so just resend to 0 results page and everything empty     
         for($i=$initialPosition;$i<$lastCounterPosition;$i++){
             if(isset($data[$i])){
                 $paginationData[0][] = $data[$i];
@@ -35,13 +34,11 @@ function getPaginationData ($data, $resultNum, $currentPage){
         $paginationData[1] = $lastPage;
         return $paginationData;
     }else{    
-        //Last Page by default will be 1 if is not results and no more than one page on the division
+        $paginationData[0] = null;
         if($lastPage==0){
             $lastPage=1;
         }
-        
-        //Redirection to get the next request on a right url with null return
-        header ("Location: ?page=".$lastPage);
-        return null;
+        $paginationData[1] = $lastPage;
+        return $paginationData;
     }
 }
